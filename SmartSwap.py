cd4f6b6ad15adcce90ff902d3aec9155e2d69309
osaybtw <teyretteb@gmail.com>
2026-06-12 15:59:37 -0300

    v1.1 + opcao 2

--- SmartSwap.py
+++ SmartSwap.py
@@ -17,6 +17,7 @@ while True:
 
     print("===== CHARGEGRID =====\n")
     print("1 - Registrar troca de bateria")
+    print("2 - Finalizar recarga de uma bateria")
     print("0 - Sair")
 
     opcao = input("Escolha: ")
@@ -33,14 +34,14 @@ while True:
 
         energia_restante = float(
             input(
-                "Informe a porcentagem restante da bateria devolvida (0-100): \n"
+                "Informe a porcentagem restante da bateria devolvida (0-100): "
             )
         )
 
         energia_utilizada = 100 - energia_restante
         valor = energia_utilizada * TARIFA
 
-        print("===== COBRANÇA =====\n")
+        print("\n===== COBRANÇA =====\n")
         print(f"Energia utilizada: {energia_utilizada:.1f}%")
         print(f"Valor a pagar: R$ {valor:.2f}")
 
@@ -51,10 +52,45 @@ while True:
         if pagamento == "1":
             baterias_disponiveis -= 1
             baterias_carregando.append(energia_restante)
+
             print("Troca realizada com sucesso!\n")
             mostrar_status()
 
         else:
             print("Operação cancelada.\n")
+
+    elif opcao == "2":
+
+        if len(baterias_carregando) == 0:
+            print("Não há baterias em carregamento.\n")
+            continue
+
+        print("===== BATERIAS EM CARREGAMENTO =====\n")
+
+        for i, carga in enumerate(baterias_carregando, start=1):
+            print(f"{i} - Bateria com {carga}% de carga")
+
+        try:
+            escolha = int(
+                input(
+                    "Digite o número da bateria que terminou de carregar: \n"
+                )
+            )
+
+            if escolha < 1 or escolha > len(baterias_carregando):
+                print("Número inválido.\n")
+                continue
+
+            baterias_carregando.pop(escolha - 1)
+            baterias_disponiveis += 1
+
+            print("Bateria recarregada com sucesso!\n")
+            print("Adicionada ao estoque de baterias disponíveis.")
+
+            mostrar_status()
+
+        except ValueError:
+            print("Digite um número válido.\n")
+
     else:
         print("Opção inválida.\n")
